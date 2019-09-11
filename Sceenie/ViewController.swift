8d39fd9434866497214ab5e5fead3833b3442032
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-11 20:59:11 +0200

    lesson1 initial commit

---
+++ Sceenie/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sceenie
+//
+//  Created by Molnár Csaba on 2019. 09. 11..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+import ReplayKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var statusLbl: UILabel!
+    @IBOutlet weak var imagePicker: UISegmentedControl!
+    @IBOutlet weak var selectedImageView: UIImageView!
+    @IBOutlet weak var micToggle: UISwitch!
+    @IBOutlet weak var recordBtn: UIButton!
+    
+
+    @IBAction func imagePicked(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func recordBtnWasPressed(_ sender: Any) {
+    }
+}
+
